656d6767e434cadb3fac26da1a17a90d74be85c2
jeplasenciap <jeplasenciap@gmail.com>
2018-06-20 17:07:13 +0100

    Corregido error en documentación automática

--- doc/conf.py
+++ doc/conf.py
@@ -170,5 +170,5 @@ todo_include_todos = True
 
 autodoc_member_order = 'bysource'
 
-autodoc_mock_imports = ['click', 'eventlet', 'Flask', 'Flask-SocketIO', 'greenlet', 'itsdangerous', 'Jinja2', 'MarkupSafe', 'pycrypto', 'python-engineio', 'python-socketio', 'pyzmq', 'six', 'Werkzeug', 'zmq']
+autodoc_mock_imports = ['click', 'eventlet', 'Flask', 'Flask-SocketIO', 'greenlet', 'itsdangerous', 'Jinja2', 'MarkupSafe', 'pycrypto', 'python-engineio', 'python-socketio', 'pyzmq', 'six', 'Werkzeug', 'zmq', 'gmpy2', 'apscheduler']
 
